0f04895e3e0264e3cda5d9310e02161ef4c3cc31
thraneh <thraneh@gmail.com>
2022-03-16 07:22:40 +0100

    rename string (from string_buffer) + chore

--- src/roq/okx/config.hpp
+++ src/roq/okx/config.hpp
@@ -51,14 +51,14 @@ class Config final : public server::Config, public server::ConfigReader::Handler
 template <>
 struct fmt::formatter<roq::okx::Config> {
   template <typename Context>
-  constexpr auto parse(Context &ctx) {
-    return std::begin(ctx);
+  constexpr auto parse(Context &context) {
+    return std::begin(context);
   }
   template <typename Context>
-  auto format(const roq::okx::Config &value, Context &ctx) {
+  auto format(const roq::okx::Config &value, Context &context) {
     using namespace std::literals;
     return fmt::format_to(
-        ctx.out(),
+        context.out(),
         R"({{)"
         R"(symbols={}, )"
         R"(accounts=[{}], )"
