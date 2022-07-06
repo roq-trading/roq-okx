1bfc3199a83f9c2b5cecba919d80fdf855139483
thraneh <thraneh@gmail.com>
2022-07-06 16:35:03 +0200

    fmt9

--- src/roq/okx/config.hpp
+++ src/roq/okx/config.hpp
@@ -55,7 +55,7 @@ struct fmt::formatter<roq::okx::Config> {
     return std::begin(context);
   }
   template <typename Context>
-  auto format(roq::okx::Config const &value, Context &context) {
+  auto format(roq::okx::Config const &value, Context &context) const {
     using namespace std::literals;
     return fmt::format_to(
         context.out(),
