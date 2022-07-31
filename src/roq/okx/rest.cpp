e89c24e3d8db140dbef8c9abf8ea9fbedfc804fe
thraneh <thraneh@gmail.com>
2022-07-31 16:17:58 +0200

    web/http clean-up

--- src/roq/okx/rest.cpp
+++ src/roq/okx/rest.cpp
@@ -154,7 +154,7 @@ void Rest::get_orders() {
         .method = method,
         .path = path,
         .query = {},
-        .accept = web::http::Accept::JSON,
+        .accept = web::http::Accept::APPLICATION_JSON,
         .content_type = {},
         .headers = headers,
         .body = {},
