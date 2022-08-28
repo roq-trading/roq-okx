c08dafca64f3a44e6b144d516b1a0e4ac4e423d9
thraneh <thraneh@gmail.com>
2022-08-28 07:06:04 +0200

    update

--- src/roq/okx/live.sh
+++ src/roq/okx/live.sh
@@ -19,9 +19,9 @@ WS_URI="wss://ws.$URI:8443/ws/v5"
 $PREFIX ./roq-okx \
   --name "okx" \
   --config_file "$CONFIG_FILE" \
-  --event_log_dir "$HOME/var/lib/roq/data" \                                                                            
-  --event_log_symlink \                                                                                                 
-  --client_listen_address "$HOME/run/$NAME.sock" \                                                                      
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --event_log_symlink \
+  --client_listen_address "$HOME/run/$NAME.sock" \
   --metrics_listen_address "$HOME/run/${NAME}_metrics.sock" \
   --ws_public_uri "$WS_URI/public" \
   --ws_private_uri "$WS_URI/private" \
