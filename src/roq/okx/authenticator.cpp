43c85a335c786b91dd8ae0587b1f06e1d617805d
thraneh <thraneh@gmail.com>
2023-04-03 20:25:21 +0200

    formatting

--- src/roq/okx/authenticator.cpp
+++ src/roq/okx/authenticator.cpp
@@ -12,10 +12,8 @@ namespace okx {
 // === IMPLEMENTATION ===
 
 Authenticator::Authenticator(Config const &config, std::string_view const &account)
-    : account_{account}, crypto_{
-                             config.get_api_key(account_),
-                             config.get_secret(account_),
-                             config.get_passphrase(account_)} {
+    : account_{account},
+      crypto_{config.get_api_key(account_), config.get_secret(account_), config.get_passphrase(account_)} {
 }
 
 std::string Authenticator::create_headers(
