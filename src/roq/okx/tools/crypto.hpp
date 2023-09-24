371692f6b67f9ad30f7d9f230625f934889c8ed9
thraneh <thraneh@gmail.com>
2023-09-24 15:15:21 +0200

    refactor

--- src/roq/okx/tools/crypto.hpp
+++ src/roq/okx/tools/crypto.hpp
@@ -9,7 +9,7 @@
 
 #include "roq/web/http/method.hpp"
 
-#include "roq/core/mac/hmac.hpp"
+#include "roq/utils/mac/hmac.hpp"
 
 namespace roq {
 namespace okx {
@@ -34,7 +34,7 @@ struct Crypto final {
       std::chrono::milliseconds timestamp);
 
  private:
-  using MAC = core::mac::HMAC<core::hash::SHA256>;
+  using MAC = utils::mac::HMAC<utils::hash::SHA256>;
   using Digest = std::array<std::byte, MAC::DIGEST_LENGTH>;
 
   std::string const key_;
