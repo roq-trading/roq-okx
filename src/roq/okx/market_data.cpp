2fd010f6c835cc49618d5da7f4b1d97cd5f7b39a
thraneh <thraneh@gmail.com>
2024-09-03 05:13:58 +0200

    chore

--- src/roq/okx/market_data.cpp
+++ src/roq/okx/market_data.cpp
@@ -10,13 +10,11 @@
 #include "roq/utils/safe_cast.hpp"
 #include "roq/utils/update.hpp"
 
-#include "roq/core/charconv.hpp"
+#include "roq/web/socket/client.hpp"
 
 #include "roq/core/tools/exception.hpp"
 
 #include "roq/core/metrics/factory.hpp"
-
-#include "roq/web/socket/client.hpp"
 
 #include "roq/okx/json/map.hpp"
 #include "roq/okx/json/utils.hpp"
