068b7f18f08a9cdd568a96d5a733fc0deac8f0ba
thraneh <thraneh@gmail.com>
2025-03-13 10:09:20 +0100

    ClOrdID can now use 2 bits to encode the API of an exchange (#489)

--- src/roq/okx/application.cpp
+++ src/roq/okx/application.cpp
@@ -11,13 +11,19 @@ using namespace std::literals;
 namespace roq {
 namespace okx {
 
+// === CONSTANTS ===
+
+namespace {
+uint8_t const API_2 = {};
+}
+
 // === IMPLEMENTATION ===
 
 int Application::main(args::Parser const &args) {
   Settings settings{args};
   Config config{settings};
   auto context = server::create_io_context(settings);
-  server::Trading<Gateway>{settings, config, *context}.dispatch();
+  server::Trading<Gateway>{settings, config, *context, API_2}.dispatch();
   return EXIT_SUCCESS;
 }
 
