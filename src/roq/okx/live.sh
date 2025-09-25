cd38cbd7c8ae8e75e7e1a0fe543349de418c36c4
thraneh <thraneh@gmail.com>
2025-09-25 05:06:00 +0200

    update launch scripts

--- src/roq/okx/live.sh
+++ src/roq/okx/live.sh
@@ -12,23 +12,14 @@ CONFIG="${CONFIG:-$NAME}"
 
 CONFIG_FILE="$ROQ_CONFIG_PATH/roq-okx/$CONFIG.toml"
 
-URI="okx.com"
+FLAGFILE="../../../share/flags/prod/flags.cfg"
 
 $PREFIX ./roq-okx \
   --name "okx" \
   --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
   --cache_dir "$HOME/var/lib/roq/cache" \
   --event_log_dir "$HOME/var/lib/roq/data" \
-  --event_log_symlink true \
   --client_listen_address "$HOME/run/$NAME.sock" \
   --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
-  --rest_uri "https://www.$URI" \
-  --ws_public_uri "wss://ws.$URI:8443/ws/v5/public" \
-  --ws_private_uri "wss://ws.$URI:8443/ws/v5/private" \
-  --ws_business_uri "wss://ws.$URI:8443/ws/v5/business" \
   $@
-
-#  --time_series_interval "60s" \
-#  --time_series_lookback "2h" \
-#  --time_series_realtime true \
-#  --download_time_series true \
