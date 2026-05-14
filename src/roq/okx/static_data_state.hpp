3353cf7f46fa977a0030f9e24bd057b2f392e1f5
thraneh <thraneh@gmail.com>
2026-05-14 02:50:28 +0000

    auto-generated connection state is unnecessary

---
+++ src/roq/okx/static_data_state.hpp
@@ -0,0 +1,17 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include <cstdint>
+
+namespace roq {
+namespace okx {
+
+enum class StaticDataState : uint8_t {
+  UNDEFINED = 0,
+  LOGIN,
+  DONE,
+};
+
+}  // namespace okx
+}  // namespace roq
